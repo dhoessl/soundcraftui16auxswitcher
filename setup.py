67d89ad9d36bcbc9012cf937e7abe8cc9ea18741
Dominic Hößl <dominic.hoessl@noris.de>
2026-07-15 15:07:41 +0200

    update requirements
    Signed-off-by: Dominic Hößl <dominic.hoessl@noris.de>

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
     package_data={},
     include_package_data=True,
     install_requires=[
-        "soundcraftui16mqtt @ git+https://github.com/dhoessl/soundcraftui16mqtt.git"
+        "soundcraftui16mqtt @ git+https://github.com/dhoessl/soundcraftui16mqtt.git",
+        "RPi.GPIO"
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
